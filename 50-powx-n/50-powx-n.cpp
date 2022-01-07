ed8c48a948bc1d0d3aa3acf601a40c20a71f416e
Ankit Kandpal <68969860+ak-create@users.noreply.github.com>
2022-01-07 20:20:52 +0530

    Time: 0 ms (100.00%), Space: 6 MB (41.37%) - LeetHub

---
+++ 50-powx-n/50-powx-n.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    
+   double  mynegpow(double x,long long n){
+        if(n==0)
+            return 1;
+        double ans=mypospow(x,n/2);
+        if(n%2==0){
+            return (1/(ans*ans));
+        }
+        else{
+            return (1/(x*ans*ans));
+        }
+    }
+    
+    
+ double  mypospow(double x,long long n){
+        if(n==0)
+            return 1;
+        double ans=mypospow(x,n/2);
+        if(n%2==0){
+            return ans*ans;
+        }
+        else{
+            return x*ans*ans;
+        }
+    }
+    
+    double myPow(double x, int n) {
+        long long N=n;
+        if(n>=0){
+            return mypospow(x,N);
+        }
+        else{
+            return mynegpow(x,-N);
+        }
+        
+    }
+};
