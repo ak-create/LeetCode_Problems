fdf7612430f66823c98fac9afe3001322ad4a1d6
Ankit Kandpal <68969860+ak-create@users.noreply.github.com>
2022-01-22 23:39:57 +0530

    Time: 40 ms (70.81%), Space: 8 MB (43.29%) - LeetHub

--- 1510-stone-game-iv/1510-stone-game-iv.cpp
+++ 1510-stone-game-iv/1510-stone-game-iv.cpp
@@ -1,13 +1,7 @@
 class Solution {
 public:
     bool winnerSquareGame(int n) {
-        vector<long long>sq(n+1,0);
-
-        for(long long i=1;i<n;i++){
-            if(i*i<=n)
-                sq[i*i]=1;
-        }
-        
+      
         vector<int>dp(n+5,-1);
         dp[0]=0;
         dp[1]=1;
@@ -23,10 +17,7 @@ public:
                 }
                     
             }
-            if(flag)
-            dp[i]=1;
-            else
-                dp[i]=0;
+            dp[i]=(flag==1)?1:0;
         }
         return dp[n];
         
