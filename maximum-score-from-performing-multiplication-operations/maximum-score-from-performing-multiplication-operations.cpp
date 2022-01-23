4c0b9788a500229b86a9c089e27662d90188922e
Ankit Kandpal <68969860+ak-create@users.noreply.github.com>
2022-01-23 18:46:58 +0530

    Time: 352 ms, Memory: 87.3 MB - LeetHub

--- maximum-score-from-performing-multiplication-operations/maximum-score-from-performing-multiplication-operations.cpp
+++ maximum-score-from-performing-multiplication-operations/maximum-score-from-performing-multiplication-operations.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     int dp[1005][1005];
-    int recdp(vector<int>&nums,vector<int>&multipliers,int i,int l,int r){
+    int recdp(vector<int>&nums,vector<int>&multipliers,int i,int l){
         
-        if(l>r)
-            return 0;
+        // if(l>r)
+        //     return 0;
         if(i==multipliers.size())
             return 0;
         
         if(dp[i][l]!=-1)
             return dp[i][l];
         
-        int opt1=multipliers[i]*nums[l]+recdp(nums,multipliers,i+1,l+1,r);
-        int opt2=multipliers[i]*nums[r]+recdp(nums,multipliers,i+1,l,r-1);
+        int opt1=multipliers[i]*nums[l]+recdp(nums,multipliers,i+1,l+1);
+        int opt2=multipliers[i]*nums[nums.size()-(i-l)-1]+recdp(nums,multipliers,i+1,l);
         
         return dp[i][l]=max(opt1,opt2);
         
@@ -23,6 +23,6 @@ public:
         //int dp[m+5][m+5];
         memset(dp,-1,sizeof(dp));
        
-        return recdp(nums,multipliers,0,0,n-1);
+        return recdp(nums,multipliers,0,0);
     }
 };
