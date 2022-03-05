4352119a697f65aaf57cf336fac8da87e1eee76e
Ankit Kandpal <68969860+ak-create@users.noreply.github.com>
2022-03-05 07:59:42 +0530

    Time: 94 ms (5.95%), Space: 52.5 MB (5.02%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.cpp
+++ 740-delete-and-earn/740-delete-and-earn.cpp
@@ -1,36 +1,18 @@
 class Solution {
 public:
+    
     int deleteAndEarn(vector<int>& nums) {
-        map<int,int>mp;
-        vector<int>freq(1e4+1,0);
-        int n=nums.size();
-        for(int i=0;i<n;i++){
-            mp[nums[i]]++;
-           freq[nums[i]]++;
+        vector<int>freq(1e5,0);
+        for(int i=0;i<nums.size();i++){
+            freq[nums[i]]++;
         }
-        auto it=mp.begin();
-        // vector<int>pre(mp.size(),0);
-        // pre[0]=it->first*it->second;
-        // it++;
-      //  int i=1;
-        vector<int>po;
-        while(it!=mp.end()){
-            // pre[i]=it->first*it->second+((i-2<0)?0:pre[i-2]);
-            po.push_back(it->first);
-            it++;
-            //i++;
+        vector<int>dp(1e5,0);
+        dp[1]=freq[1];
+        for(int i=2;i<1e5;i++){
+            dp[i]=max((freq[i]*i)+dp[i-2],dp[i-1]);
+          //  cout<<dp[i]<<" ";
         }
-        
-       vector<int>dp(mp.size(),0);
-        dp[0]=po[0]*freq[po[0]];
-        
-        for(int i=1;i<mp.size();i++){
-            if(po[i]-po[i-1]==1)
-            dp[i]=max((i-2<0?0:dp[i-2])+po[i]*freq[po[i]],dp[i-1]);
-            else
-                dp[i]=(dp[i-1]+po[i]*freq[po[i]]);
-        }
-        return dp[mp.size()-1];
-        
+        //cout<<endl;
+        return max(dp[1e5-1],dp[1e5-2]);
     }
 };
