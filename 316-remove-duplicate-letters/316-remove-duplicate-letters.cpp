dd919c210e71ba65624dc20da5189138f63f4c42
Ankit Kandpal <68969860+ak-create@users.noreply.github.com>
2022-03-18 23:26:11 +0530

    Time: 3 ms (73.49%), Space: 6.7 MB (66.29%) - LeetHub

--- 316-remove-duplicate-letters/316-remove-duplicate-letters.cpp
+++ 316-remove-duplicate-letters/316-remove-duplicate-letters.cpp
@@ -8,33 +8,26 @@ public:
         }
         
         string ans="";
-        stack<char>st;
-        st.push(s[0]);
+        ans.push_back(s[0]);
         vis[s[0]-'a']=1;
         freq[s[0]-'a']--;
         for(int i=1;i<s.size();i++){
             
             if(vis[s[i]-'a']==0)
             {
-            while(st.size()>=1&&st.top()>s[i]&&freq[st.top()-'a']>=1)
+            while(ans.size()>=1&&ans.back()>s[i]&&freq[ans.back()-'a']>=1)
             {
-                vis[st.top()-'a']=0;
-                st.pop();
+                vis[ans.back()-'a']=0;
+                ans.pop_back();
                 
             }
-            st.push(s[i]);
+            ans.push_back(s[i]);
             vis[s[i]-'a']=1;
             }    
             freq[s[i]-'a']--;
            
         }
         
-        while(st.size())
-        {
-            ans.push_back(st.top());
-            st.pop();
-        }
-        reverse(ans.begin(),ans.end());
         return ans;
         
     }
