5cb8dc77fd5dada0eb839c06521af09b46417a83
Ankit Kandpal <68969860+ak-create@users.noreply.github.com>
2022-06-28 11:17:49 +0530

    Time: 76 ms (14.43%), Space: 71.5 MB (49.56%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.cpp
+++ 2-add-two-numbers/2-add-two-numbers.cpp
@@ -10,28 +10,58 @@
  */
 class Solution {
 public:
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-          ListNode *dummy = new ListNode(0);
-        ListNode *curr = dummy;
-        int carry = 0;
-        
-        while(l1 != NULL || l2 != NULL || carry == 1){
-            int sum = 0;
-            if(l1 != NULL){
-                sum += l1->val;
-                l1 = l1->next;
-            }
-            if(l2 != NULL){
-                sum += l2->val;
-                l2 = l2->next;
-            }
-            sum += carry;
-            carry = sum/10;
-            ListNode *node = new ListNode(sum % 10);
-            curr->next = node;
-            curr = curr->next;
+    ListNode* reverse(ListNode* head)
+    {
+        ListNode* prev=NULL,*curr=head,*nxt=head->next;
+        while(curr)
+        {
+            curr->next=prev;
+            prev=curr;
+            curr=nxt;
+            if(nxt)
+                nxt=nxt->next;
         }
+        return prev;
+    }
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        
+        ListNode* curr1=l1,*curr2=l2;
+        int carry=0;
+        ListNode* dummy=new ListNode(-1);
+        ListNode* tail=dummy;
+        while(curr1 or curr2)
+        {
+            int val1=0,val2=0;
+            
+            if(curr1!=NULL){
+                val1=curr1->val;
+                curr1=curr1->next;
+            }
+            if(curr2!=NULL){
+                val2=curr2->val;
+                curr2=curr2->next;
+            }
+            
+            int sum=val1+val2+carry;
+            if(sum>=10)
+            {
+                carry=1;
+                sum=sum%10;
+            }
+            else
+                carry=0;
+            ListNode* newnode=new ListNode(sum);
+            tail->next=newnode;
+            tail=tail->next;
+            
+            
+        }
+        if(carry){
+             ListNode* newnode=new ListNode(1);
+            tail->next=newnode;}
+            
         return dummy->next;
-    
+       // ListNode* nhead=reverse(dummy->next);
+       //  return nhead;
     }
 };
