77725431efbcc3ff51a2b0060673d738539830d4
Ankit Kandpal <68969860+ak-create@users.noreply.github.com>
2022-07-15 06:09:09 +0530

    Time: 32 ms (47.07%), Space: 24.8 MB (20.36%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.cpp
+++ 695-max-area-of-island/695-max-area-of-island.cpp
@@ -15,15 +15,15 @@ public:
             }
         }
         int ans=0;
-        set<pair<int,int>>us;
+       // set<pair<int,int>>us;
         queue<pair<int,int>>q;
         int temp=0;
        // cout<<dx.size()<<endl;
         for(int i=0;i<island.size();i++){
             temp=0;
-        if(us.find(island[i])==us.end()){
+        if(grid[island[i].first][island[i].second]==1){
         q.push(island[i]);
-           us.insert(island[i]);
+         grid[island[i].first][island[i].second]=-1;
         while(q.size())
         {
             int sz=q.size();
@@ -36,9 +36,10 @@ public:
                 {
                     if(curr.first+dx[ind]<m and curr.first+dx[ind]>=0 and 
                        curr.second+dy[ind]<n and curr.second+dy[ind]>=0 and 
-                       grid[curr.first+dx[ind]][curr.second+dy[ind]]==1 and us.find({curr.first+dx[ind],curr.second+dy[ind]})==us.end()){
+                       grid[curr.first+dx[ind]][curr.second+dy[ind]]==1 ){
                         q.push({curr.first+dx[ind],curr.second+dy[ind]});
-                     us.insert({curr.first+dx[ind],curr.second+dy[ind]}); 
+                        grid[curr.first+dx[ind]][curr.second+dy[ind]]=-1;
+                   //  us.insert({curr.first+dx[ind],curr.second+dy[ind]}); 
                     }
                 }
                 
