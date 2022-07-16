a8ad04f7b5a2ec432a6f777bd058d92b6016130e
Ankit Kandpal <68969860+ak-create@users.noreply.github.com>
2022-07-16 07:52:40 +0530

    Time: 10 ms (99.04%), Space: 13.7 MB (43.43%) - LeetHub

---
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    struct comp
+    {
+      constexpr  bool operator()(pair<int,int> const &p1,pair<int,int> const &p2)
+          const noexcept{
+            return p1.second<p2.second;}
+    };
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+     unordered_map<int,int>mp;
+        for(int ele:nums)
+            mp[ele]++;
+        priority_queue<pair<int,int>,vector<pair<int,int>>,comp>pq;
+        
+        for(auto x:mp)
+        {
+            pq.push({x.first,x.second});
+        }
+        
+        vector<int>topK;
+        for(int i=0;i<k;i++)
+        {
+            topK.push_back(pq.top().first);
+            pq.pop();
+        }
+        return topK;
+    }
+};
