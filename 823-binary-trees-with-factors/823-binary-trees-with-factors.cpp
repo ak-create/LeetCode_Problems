283123c9fad32ee08350308bb27ed31a920bb9e6
Ankit Kandpal <68969860+ak-create@users.noreply.github.com>
2022-08-09 21:23:02 +0530

    Time: 379 ms (9.84%), Space: 9.3 MB (54.77%) - LeetHub

--- 823-binary-trees-with-factors/823-binary-trees-with-factors.cpp
+++ 823-binary-trees-with-factors/823-binary-trees-with-factors.cpp
@@ -1,19 +1,36 @@
 class Solution {
 public:
-  
-      
-    int numFactoredBinaryTrees(vector<int>& A) {
-        long res = 0, mod = pow(10, 9) + 7;
-        sort(A.begin(), A.end());
-        unordered_map<int, long> dp;
-        for (int i = 0; i < A.size(); ++i) {
-            dp[A[i]] = 1;
-            for (int j = 0; j < i; ++j)
-                if (A[i] % A[j] == 0)
-                    dp[A[i]] = (dp[A[i]] + dp[A[j]] * dp[A[i] / A[j]]) % mod;
-            res = (res + dp[A[i]]) % mod;
+    int numFactoredBinaryTrees(vector<int>& arr) {
+        vector<long long int>prod;
+        sort(arr.begin(),arr.end());
+        unordered_map<int,int>s;
+        for(int i=0;i<arr.size();i++)
+        {
+            s[arr[i]]=i;
+            // for(int j=i;j<arr.size();j++)
+            // {
+            //     prod.push_back((long long int )arr[i]*arr[j]);
+            // }
         }
-        return res;
+        vector<long long int>dp(arr.size()+1,1);
+        const int mod=1e9+7;
+        dp[0]=1;
+        long long int ans=1;
+        for(int i=1;i<arr.size();i++)
+        {
+            long long int temp=1;
+            for(int j=i;j>=0;j--)
+            {
+                if(s.find(arr[i]/arr[j])!=s.end() and arr[i]%arr[j]==0)
+                    temp+=(dp[j]*dp[s[arr[i]/arr[j]]])%mod;
+            }
+           dp[i]=(temp)%mod;
+            ans+=(temp)%mod;
+        }
+        
+        return ans%mod;
+        
+        
+        
     }
-    
 };
