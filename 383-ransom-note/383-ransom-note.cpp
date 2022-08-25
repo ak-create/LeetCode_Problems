4074eb1620080c0bf98f545bbfa98b0458c22348
Ankit Kandpal <68969860+ak-create@users.noreply.github.com>
2022-08-25 09:10:58 +0530

    Time: 26 ms (52.04%), Space: 8.9 MB (39.49%) - LeetHub

--- 383-ransom-note/383-ransom-note.cpp
+++ 383-ransom-note/383-ransom-note.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
-        vector<int>v(26,0),vv(26,0);
+        vector<int>v(26,0);
         for(int i=0;i<magazine.size();i++)
             v[magazine[i]-'a']++;
         
-        for(int i=0;i<ransomNote.size();i++)
-            vv[ransomNote[i]-'a']++;
-        
-         for(int i=0;i<26;i++)
-         if(v[i]<vv[i])
-             return false;
+        for(int i=0;i<ransomNote.size();i++){
+            if(v[ransomNote[i]-'a']==0)
+                return false;
+            v[ransomNote[i]-'a']--;
+            
+        }
         
         return true;
     }
