2b48caeeadf3fd4315d38981fe1db674c8be52be
Ankit Kandpal <68969860+ak-create@users.noreply.github.com>
2022-09-09 12:16:32 +0530

    Time: 1437 ms (18.30%), Space: 125.1 MB (91.78%) - LeetHub

--- 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.cpp
+++ 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.cpp
@@ -4,7 +4,7 @@ public:
     static bool mycmp(vector<int>&v1,vector<int>&v2)
      {
          if(v1[0]==v2[0])
-             return v1[1]<v2[1];
+             return v1[1]>v2[1];
          return v1[0]<v2[0];
      }
     
@@ -13,26 +13,18 @@ public:
         int n=properties.size();
         
         int mx=properties[n-1][1];
-        int mf=properties[n-1][0];
-        int prevmx=0;
         int ans=0;
         for(int i=n-2;i>=0;i--)
         {
-           if(properties[i][0]!=mf)
-           {
                if(properties[i][1]<mx)
                    ans++;
                else if(properties[i][1]>mx)
-               {
-                   prevmx=mx;
                    mx=properties[i][1];
-                   mf=properties[i][0];
-               }
+               
            }
-            else if(properties[i][1]<prevmx)
-                ans++;
+           
                 
-        }
+        
         return ans;
     }
 };
